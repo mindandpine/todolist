db0582ae5724789d628c686f15aa9fafc020207a
mindandpine <casey.aviles@mindandpine.com>
2023-07-14 22:03:36 +0200

    added first iteration of todo

--- README.md
+++ README.md
@@ -8,5 +8,14 @@
 4. make a function that print()s the todolist entry
 5. pass a variable to the function
 6. make a new function that writes the entries to the list
-7. add a command that adds an index number [1,2,3,...] to the entry
 
+---
+
+
+7. add a command that adds an index number [1,2,3,...] to the entry
+8. Make the todo list add, remove and clear all the entries
+9. change(or copy and edit) the add, remove and clear all functions to do SQL instead of python
+10. Ask luke for a database
+11. Connect to database from python
+12. run functions see if it works
+13. add a function to Read from the database
---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys
+ 
+
+"""new entry adds an entry to an existing file"""
+def new_entry(entry):
+    todo_list = open("Todolist.txt", "a")
+    todo_list.writelines(entry)
+    todo_list.close()
+
+"""this lets you read from the terminal https://www.geeksforgeeks.org/take-input-from-stdin-in-python/"""
+def read_from_stdin():
+    for line in sys.stdin:
+        if 'q' == line.rstrip():
+            break
+        try:
+            new_entry(line)
+            print(f'New Entry : {line}')
+        except:
+            print("failed to append to file")
+
+
+
+"""if name is main is a way to run the script
+from the terminal https://stackoverflow.com/questions/419163/what-does-if-name-main-do"""
+if __name__ == "__main__":
+    print(" Todo app\n Please enter new todo items. \n Press 'q + <Enter>' to exit")
+    read_from_stdin()
